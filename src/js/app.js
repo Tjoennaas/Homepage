bfe8dc0337e4ad212d23e67044eb88b31e89e6ad
Tjoennaas <145026954+Tjoennaas@users.noreply.github.com>
2024-07-08 11:38:49 +0200

    Add function for navigate to each of the items.

--- src/js/app.js
+++ src/js/app.js
@@ -1,6 +1,7 @@
 
 
 
+
 const navigateButtonEspen = document.querySelector(".navigate-button-espen");
 const navigateButtonAdele = document.querySelector(".navigate-button-adele");
 const frontpageForAdele = document.querySelector(".frontpage-for-adele");
@@ -14,6 +15,8 @@ const renderItemsAdele = document.querySelector(".render-items-adele");
 const renderItemsEspen = document.querySelector(".render-items-espen");
 const filterPainting = document.querySelector(".filter-painting");
 const filterDga = document.querySelector(".filter-dga");
+const mainContent = document.querySelector("body");  //change this
+
 
 
 function displayGalleryEspen() {
@@ -23,88 +26,102 @@ function displayGalleryEspen() {
     filterAdele.style.display = "none";
     filterEspen.style.display = "flex";
     linkToFrontpage.style.display = "none";
-    linkFrontPageArtist.style.display = "flex"; 
+    linkFrontPageArtist.style.display = "flex";
 }
 
+
+
 function displayGalleryAdele() {
+    const itemDetails = document.querySelector(".item-details");
+    if (itemDetails) {
+        itemDetails.remove();
+    }
     artists.style.display = "none";
     frontpageForAdele.style.display = "flex";
     frontpageForEspen.style.display = "none";
     filterAdele.style.display = "flex";
     filterEspen.style.display = "none";
     linkToFrontpage.style.display = "none";
-    linkFrontPageArtist.style.display = "flex"; 
-     renderCategoryTwo();
-     renderCategoryOne();
+    linkFrontPageArtist.style.display = "flex";
+    renderCategoryTwo();
+    renderCategoryOne();
 }
 
+
 function backToHomepage() {
-    artists.style.display = "flex";
+    document.querySelector(".frontpage").style.display = "block";
     frontpageForAdele.style.display = "none";
     frontpageForEspen.style.display = "none";
-    filterAdele.style.display = "none";
-    filterEspen.style.display = "none";
+    const itemDetails = document.querySelector(".item-details");
+    if (itemDetails) {
+        itemDetails.remove();
+    }
+    artists.style.display = "flex";
     linkToFrontpage.style.display = "flex";
     linkFrontPageArtist.style.display = "none";
 }
+
 
 linkFrontPageArtist.addEventListener("click", backToHomepage);
 navigateButtonEspen.addEventListener("click", displayGalleryEspen);
 navigateButtonAdele.addEventListener("click", displayGalleryAdele);
 
-/* ----------- ES6 CLASSES -------------- */
 
 class ItemAdele {
-    constructor(title, technique, size, year, img) {
+    constructor(img, title, technique, size, button, year ) {
         this.title = title;
         this.technique = technique;
         this.size = size;
         this.year = year;
         this.img = img;
+        this.button = button;  
     }
 }
 
 class ItemAdelePrint extends ItemAdele {
-    constructor(title, technique, size, year, img, print) {
-        super(title, technique, size, year, img);
+      constructor(img, title, technique, size, year, button, print ) {
+      super(img, title, technique, size, button, year );
         this.print = print;
     }
 }
 
+
 const itemsAdele = {
     category1: [
-        new ItemAdele("Kreation", "Oil painting", "", "2020", "../src/assets/adele1/a1.jpg"),
-        new ItemAdele("Ut", "Oil painting", "", "2020", "../src/assets/adele1/a3.jpg"),
-        new ItemAdele("Danum valley", "Oil painting", "", "2019", "../src/assets/adele1/a4.jpg"),
-        new ItemAdele("Thamel", "Oil painting", "", "2019", "../src/assets/adele1/a5.jpg"),
-        new ItemAdele("Kjøttmeis", "Oil painting", "", "2018", "../src/assets/adele1/a6.jpg"),
-        new ItemAdele("", "Oil painting", "", "2018", "../src/assets/adele1/a7.jpg"),
-        new ItemAdele("", "Oil painting", "", "2018", "../src/assets/adele1/a10.jpg"),
-        new ItemAdele("", "Oil painting", "", "2018", "../src/assets/adele1/a11.jpg"),
-        new ItemAdele("", "Oil painting", "", "2018", "../src/assets/adele1/a16.jpg"),
-        new ItemAdele("", "Oil painting", "", "2018", "../src/assets/adele1/a17.jpg"),
-        new ItemAdele("", "Oil painting", "", "2018", "../src/assets/adele1/a18.jpg"),
-        new ItemAdele("", "Oil painting", "", "2018", "../src/assets/adele1/a21.jpg"),
-        new ItemAdele("", "Oil painting", "", "2018", "../src/assets/adele1/a18.jpg"),
-        new ItemAdele("Spurv", "Oil painting", "", "2018", "../src/assets/adele1/a20.jpg"),
+        new ItemAdele("../src/assets/adele1/a1.jpg", "Kreation", "Oil painting", "","More", "2020"),
+        new ItemAdele("../src/assets/adele1/a3.jpg", "Ut", "Oil painting", "", "More", "2020"),
+        new ItemAdele("../src/assets/adele1/a4.jpg","Danum valley", "Oil painting","","More","2019"),
+        new ItemAdele("../src/assets/adele1/a5.jpg", "Thamel", "Oil painting", "", "More","2019"),
+        new ItemAdele("../src/assets/adele1/a6.jpg","Kjøttmeis", "Oil painting","","More","2018"),
+        new ItemAdele("../src/assets/adele1/a7.jpg","", "Oil painting", "", "More","2018"),
+        new ItemAdele("../src/assets/adele1/a10.jpg","", "Oil painting", "","More","2018"),
+        new ItemAdele("../src/assets/adele1/a11.jpg","", "Oil painting", "","More","2018"),
+        new ItemAdele("../src/assets/adele1/a16.jpg","", "Oil painting", "","More","2018"),
+        new ItemAdele("../src/assets/adele1/a17.jpg","", "Oil painting", "","More","2018"),
+        new ItemAdele("../src/assets/adele1/a18.jpg","", "Oil painting", "", "More","2018"),
+        new ItemAdele("../src/assets/adele1/a21.jpg","", "Oil painting", "","More", "2018"),
+        new ItemAdele("../src/assets/adele1/a18.jpg","", "Oil painting", "","More","2018"),
+        new ItemAdele("../src/assets/adele1/a20.jpg","Spurv", "Oil painting", "","More","2018"),
       ],
 
     category2: [
-        new ItemAdelePrint("", "DGA", "A3", "2024", "../src/assets/adele1/a2.jpg", "50"),
-        new ItemAdelePrint("", "DGA", "A3", "2022", "../src/assets/adele1/a8.jpg", "37"),
-        new ItemAdelePrint("", "DGA", "A3", "2022", "../src/assets/adele1/a9.jpg", "50"),
-        new ItemAdelePrint("", "DGA", "A3", "2022", "../src/assets/adele1/a13.jpg", "49"),
-        new ItemAdelePrint("", "DGA", "A3", "2022", "../src/assets/adele1/a14.jpg", "50"),
-        new ItemAdelePrint("", "DGA", "A3", "2022", "../src/assets/adele1/a15.jpg", "50"),
-        new ItemAdelePrint("", "DGA", "A3", "2022", "../src/assets/adele1/a19.jpg", "50"),
-        new ItemAdelePrint("", "DGA", "A3", "2022", "../src/assets/adele1/a22.jpg", "50"),
+        new ItemAdelePrint( "../src/assets/adele1/a2.jpg","", "DGA", "A3", "2024","More","50"),
+        new ItemAdelePrint( "../src/assets/adele1/a8.jpg", "", "DGA", "A3", "2022","More","37"),
+        new ItemAdelePrint( "../src/assets/adele1/a9.jpg","", "DGA", "A3", "2022","More","50"),
+        new ItemAdelePrint( "../src/assets/adele1/a13.jpg","", "DGA", "A3", "2022","More","49"),
+        new ItemAdelePrint( "../src/assets/adele1/a14.jpg","", "DGA", "A3", "2022","More","50"),
+        new ItemAdelePrint("../src/assets/adele1/a15.jpg","", "DGA", "A3", "2022","More", "50"),
+        new ItemAdelePrint("../src/assets/adele1/a19.jpg", "", "DGA", "A3", "2022","More","50"),
+        new ItemAdelePrint( "../src/assets/adele1/a22.jpg","", "DGA", "A3", "2022","More","50"),
     ],
 };
 
-/*----------------------------------------------------------*/
 
-    class RenderItems {
-        static render(items, container) {
+
+
+class RenderItems {
+    static render(items, container) {
+        container.innerText = '';
         items.forEach(item => {
             const img = document.createElement("img");
             const listItem = document.createElement("li");
@@ -112,9 +129,11 @@ const itemsAdele = {
             const technique = document.createElement("p");
             const size = document.createElement("p");
             const year = document.createElement("p");
-           
+            const textContainer = document.createElement("div");
+            const button = document.createElement("button");
 
             listItem.className = "item-container";
+            textContainer.className = "text-container";
 
             img.src = item.img;
             img.alt = item.title;
@@ -122,39 +141,40 @@ const itemsAdele = {
             technique.innerText = `Technique: ${item.technique}`;
             size.innerText = `Size: ${item.size}`;
             year.innerText = `Year: ${item.year}`;
-         
+            button.innerText = `${item.button}`;
 
-            listItem.append(
-                  title,
-                  technique,
-                  size,
-                  year );
+            button.addEventListener("click", () => {
+                displayItemDetails(item);
+            });
+
+            textContainer.append( 
+                title,
+                technique, 
+                size,
+                year
+            );
 
             if (item instanceof ItemAdelePrint) {
-               
                 const print = document.createElement('p');
                 print.innerText = `Print: ${item.print}`;
-                listItem.append(print);
+                textContainer.append(print);
             }
 
-                listItem.append(img);
-                container.append(listItem);
+            listItem.append(img, textContainer, button);
+            container.append(listItem);
         });
     }
 }
 
-/* -------------------- filter  ---------------------------- */
-
 function renderCategoryOne() {
     renderItemsAdele.innerText = '';
-    RenderItems.render(itemsAdele.category1 , renderItemsAdele);
+    RenderItems.render(itemsAdele.category1, renderItemsAdele);
 }
 
 function renderCategoryTwo() {
     renderItemsAdele.innerText = '';
-    RenderItems.render(itemsAdele.category2 , renderItemsAdele);
+    RenderItems.render(itemsAdele.category2, renderItemsAdele);
 }
-
 
 filterPainting.addEventListener("click", () => {
     renderCategoryOne();
@@ -162,11 +182,85 @@ filterPainting.addEventListener("click", () => {
     filterDga.classList.remove("filter-dga--visible");
 });
 
-
 filterDga.addEventListener("click", () => {
     renderCategoryTwo();
     filterPainting.classList.remove("filter-painting");
     filterDga.classList.add("filter-dga--visible");
-
 });
+
+
+
+/*-----------------------------------------------------------*/
+
+
+
+function displayItemDetails(item) {
+
+    const existingDetails = document.querySelector(".item-details");
+    if (existingDetails) {
+        existingDetails.remove();
+    }
+
+  
+    const itemDetails = document.createElement("div");
+    const backButton = document.createElement("button");
+    const itemImg = document.createElement("img");
+    const itemInfo = document.createElement("div");
+    const itemTitle = document.createElement("p");
+    const itemTechnique = document.createElement("p");
+    const itemSize = document.createElement("p");
+    const itemYear = document.createElement("p");
+    const itemPrint = document.createElement("p");
+    
+ 
+    backButton.className = "back-button";
+    backButton.innerText = "Back";
+    backButton.addEventListener("click", displayGalleryAdele);
+
+   
+    itemImg.className = "item-img";
+    itemInfo.className = "item-info";
+    itemTitle.className = "item-title";
+    itemTechnique.className = "item-technique";
+    itemSize.className = "item-size";
+    itemYear.className = "item-year";
+    itemPrint.className = "item-print";
+    itemDetails.className = "item-details";
+
+    itemImg.src = item.img;
+    itemImg.alt = "Item Image";
+
+    itemTitle.innerText = `Title: ${item.title}`;
+    itemTechnique.innerText = `Technique: ${item.technique}`;
+    itemSize.innerText = `Size: ${item.size}`;
+    itemYear.innerText = `Year: ${item.year}`;
+
+    itemPrint.innerText = `Print: ${item.print || ''}`;
+    itemPrint.style.display = item.print ? "block" : "none";
+
+    itemInfo.append(
+         itemTitle,
+         itemTechnique, 
+         itemSize,
+         itemYear, 
+         itemPrint
+    );
+
+    itemDetails.append(
+       backButton,
+       itemImg, 
+       itemInfo
+    );
+
+    mainContent.appendChild(
+        itemDetails
+    );
+
+    document.querySelector(".frontpage").style.display = "none";
+    frontpageForAdele.style.display = "none";
+    frontpageForEspen.style.display = "none";
+    itemDetails.style.display = "block";
+}
+
+/*----------------------------------  Espen homepage -------------------------------------*/
 
